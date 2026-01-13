1113722da6ad160745c10b5e1367c42f35c2eacc
Jonathan Di Rico <diricojonathan@gmail.com>
2026-01-13 17:33:55 +0100

    Add config.sh file for check mode configuration
    - Created src/check/core/config.sh for check mode specific configs
    - File is optional and can be used to set defaults and feature flags
    - Updated init.sh to source config.sh if it exists

---
+++ src/check/core/config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Sonda Check Mode Configuration
+# This file can be used to set check mode specific configurations
+
+# Example: Set default verbosity level
+# DEFAULT_VERBOSITY=""
+
+# Example: Enable/disable specific features
+# ENABLE_FEATURE_X=true
